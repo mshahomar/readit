57f23126e88dd795d9c0a1bcc94cf5802ca48a27
mshah.omar <shah_omar@astro.com.my>
2016-11-22 01:04:13 +0800

    Create Class-based Views for AuthorList and Count to get only authors with book.

--- books/views.py
+++ books/views.py
@@ -1,17 +1,29 @@
+from django.db.models import Count
 from django.shortcuts import render
+from django.views.generic import View
 # from django.http import HttpResponse
 
-from .models import Book
+from .models import Book, Author
 
 
 def list_books(request):
     """List the books that have reviews."""
 
     books = Book.objects.exclude(date_reviewed__isnull=True).prefetch_related('authors')
-
     context = {'books': books}
-
     return render(request, 'books/list.html', context)
 
     # return HttpResponse(request.user.username)
 
+
+class AuthorList(View):
+
+    def get(self, request):
+        authors = Author.objects.annotate(
+            published_books=Count('books')
+        ).filter(
+            published_books__gt=0
+        )
+        context = {'authors': authors}
+
+        return render(request, "books/authors.html", context)
