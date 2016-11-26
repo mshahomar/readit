a3f50ad25cb30a5a15e108325a06cfb7384954f9
mshah.omar <shah_omar@astro.com.my>
2016-11-26 16:58:04 +0800

    ..

--- readit/settings.py
+++ readit/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
